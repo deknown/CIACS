a15983d21a82655ba27fdde9f7f096b554419e46
Pavel <ganita09@rambler.ru>
2020-06-15 18:29:35 +0300

    Solution: change .Net Core to .Net Framework

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CIACS_win_client
 {
@@ -6,7 +10,6 @@ namespace CIACS_win_client
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
 		}
 	}
 }
